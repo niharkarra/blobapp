852385b5a46e66a0ad89ad45886edccda6b9b158
502299368 <ramesh.yv@ge.com>
2016-06-17 23:25:42 -0400

    first commit

---
+++ src/main/java/com/example/BlobappApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlobappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlobappApplication.class, args);
+	}
+}
